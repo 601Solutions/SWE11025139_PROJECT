dfba6994208d788d600dc8b5047c3d20794aef3c
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2025-11-15 18:04:31 +0900

    add docstrings

--- LLM/llm_rag/config.py
+++ LLM/llm_rag/config.py
@@ -1,25 +1,27 @@
-# llm_rag/config.py
+#====================================================
+# Author: 601 Solutions
+# Title: config.py
+# 핵심 변수 설정 정의
+#====================================================
 
+"""
+프로젝트 전반에서 사용되는 핵심 설정 변수를 정의
+
+- 프로젝트 루트 경로
+- .env 파일 로드
+- 임베딩 및 LLM 모델 이름
+- Vector DB 경로
+- API 키
+"""
 
 from pathlib import Path
 import os
 import dotenv
 
-# .env 파일에서 환경 변수 로드
 dotenv.load_dotenv()
 
-# 이 파일(config.py)의 위치를 기준으로 프로젝트 루트를 계산합니다.
-# llm_rag -> SWE_PROJECT
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
-
-# Embedding
 EMBEDDING_MODEL = 'jhgan/ko-sbert-nli'
-
-# Vector DB
 DB_DIR = PROJECT_ROOT / 'persistent_chroma_db' 
-
-# LLM
 LLM_MODEL = "gemini-2.5-flash"
-
-# LLM API KEY
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
